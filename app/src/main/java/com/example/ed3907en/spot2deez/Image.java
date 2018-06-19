7619fc11f443207dd5f6670e95f82502a5e17c43
manu <emmanuel.damota@gmail.com>
2018-06-19 21:38:34 +0200

    Add Youtube music search

--- app/src/main/java/com/example/ed3907en/spot2deez/Image.java
+++
@@ -1,7 +0,0 @@
-package com.example.ed3907en.spot2deez;
-
-public class Image {
-    public int height;
-    public int width;
-    public String url;
-}
--- app/src/main/java/com/example/ed3907en/spot2deez/ProviderApi.java
+++
@@ -1,9 +0,0 @@
-package com.example.ed3907en.spot2deez;
-
-import io.reactivex.Observable;
-import io.reactivex.Single;
-
-public abstract class ProviderApi {
-
-    public abstract Single<Track> getTrack(String trackId) throws Exception;
-}
