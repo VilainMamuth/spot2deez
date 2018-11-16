7eadeaa1919e8681fe621bc0919e304473a04a14
manu <emmanuel.damota@gmail.com>
2018-11-16 16:40:37 +0100

    Added DuckDuckGo as a fallback search engine

--- app/src/main/java/net/damota/android/xmod/MainActivity.java
+++ app/src/main/java/net/damota/android/xmod/MainActivity.java
@@ -2,6 +2,7 @@ package net.damota.android.xmod;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,8 +25,13 @@ import net.damota.android.xmod.spotify.SpotifyApi;
 import net.damota.android.xmod.spotify.Token;
 import net.damota.android.xmod.spotify.TokenPersister;
 
-public class MainActivity extends AppCompatActivity implements LoadImageTask.Listener  {
+public class MainActivity extends AppCompatActivity implements LoadImageTask.Listener {
     private final static String TAG = MainActivity.class.getSimpleName();
+    private PackageManager pm;
+    private final static String PACKAGE_DEEZER = "deezer.android.app";
+    private final static String PACKAGE_YOUTUBE_MUSIC = "com.google.android.apps.youtube.music";
+    private final static String PACKAGE_AMAZON_MUSIC = "com.amazon.mp3";
+
     private static final String PROVIDER_DEEZER = "deezer";
     private final static String PROVIDER_SPOTIFY = "spotify";
     private final static String TYPE_TRACK = "track";
@@ -39,11 +45,12 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
 
 
     private TextView artistName;
-    /** Le nom peut être le titre d'une piste ou le nom d'un album */
+    /**
+     * Le nom peut être le titre d'une piste ou le nom d'un album
+     */
     private TextView name;
     private TextView duration;
     private TextView release;
-
 
 
     private AdView mAdView;
@@ -63,6 +70,8 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        this.pm = getPackageManager();
+        hideNotInstalledApps();
 
         Intent i = getIntent();
 
@@ -78,16 +87,16 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
         parseUrl(url);
 
 
-        switch (sourceProviderName){
+        switch (sourceProviderName) {
             case PROVIDER_SPOTIFY:
                 sourceApi = new SpotifyApi();
                 break;
-             case PROVIDER_DEEZER:
+            case PROVIDER_DEEZER:
                 sourceApi = new DeezerApi();
                 break;
         }
 
-        if (sourceApi.needToken()){
+        if (sourceApi.needToken()) {
             //si on a un token valide en cache , alors on en demande pas un nouveau
             Token spotifyAccessToken = TokenPersister.getToken(this);
             if (spotifyAccessToken != null) {
@@ -96,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
         }
 
 
-        switch (sourceProviderType){
+        switch (sourceProviderType) {
             case TYPE_TRACK:
                 this.getTrack(sourceApi, sourceProviderId);
                 break;
@@ -109,7 +118,6 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
         if (sourceApi.needToken()) {
             TokenPersister.setToken(this, ((SpotifyApi) sourceApi).getAccessToken());
         }
-
 
 
     }
@@ -137,6 +145,7 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
 
         }
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -149,6 +158,7 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
         duration.setText(track.getDuration());
         release.setText("");
     }
+
     private void updateActivity(Album album) {
         new LoadImageTask(this).execute(album.getCoverUrl());
         name.setText(album.getTitle());
@@ -164,18 +174,19 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
 
     /**
      * Basic parsing, need refactoring
+     *
      * @param uri
      */
-    private void parseUrl(Uri uri){
-        switch(uri.getScheme()){
-            case "spotify" :
+    private void parseUrl(Uri uri) {
+        switch (uri.getScheme()) {
+            case "spotify":
                 sourceProviderName = PROVIDER_SPOTIFY;
                 break; // optional
 
-            case "http" :
-            case "https" :
+            case "http":
+            case "https":
 
-                switch (uri.getHost()){
+                switch (uri.getHost()) {
                     case "www.spotify.com":
                     case "open.spotify.com":
                         sourceProviderName = PROVIDER_SPOTIFY;
@@ -188,12 +199,12 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
                 break; // optional
 
             // You can have any number of case statements.
-            default : //
+            default: //
                 Toast.makeText(this, "Url non conforme", Toast.LENGTH_SHORT).show();
         }
         hideSourceProvider(sourceProviderName);
 
-        switch (sourceProviderName){
+        switch (sourceProviderName) {
             case PROVIDER_SPOTIFY:
                 sourceProviderType = uri.getPathSegments().get(0);
                 sourceProviderId = uri.getLastPathSegment();
@@ -206,33 +217,36 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
 
     }
 
-    public void openApp(View view){
+    public void openApp(View view) {
         Log.d(TAG, "openApp: " + view.toString());
         Intent i = null;
-        Uri backup = null ;
-        switch (view.getId()){
+        Uri backup = null;
+        switch (view.getId()) {
             case R.id.buttonDeez:
-                 i = new Intent (Intent.ACTION_VIEW, Uri.parse("deezer://www.deezer.com/search/" + artistName.getText() + " " + name.getText()) );
-                 // ne fonctionne pas toujours backup = Uri.parse("https://www.deezer.com/search/" + artistName.getText() + " " + name.getText());
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse("deezer://www.deezer.com/search/" + artistName.getText() + " " + name.getText()));
+                // ne fonctionne pas toujours backup = Uri.parse("https://www.deezer.com/search/" + artistName.getText() + " " + name.getText());
                 break;
             case R.id.buttonSpot:
-                 i = new Intent(Intent.ACTION_VIEW, Uri.parse("spotify:search:" + artistName.getText() + " " + name.getText()) );
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse("spotify:search:" + artistName.getText() + " " + name.getText()));
                 break;
             case R.id.buttonYM:
-                 i = new Intent(Intent.ACTION_SEARCH );
-                 i.putExtra(SearchManager.QUERY, artistName.getText() + " " + name.getText())
-                 .setPackage("com.google.android.apps.youtube.music");
+                i = new Intent(Intent.ACTION_SEARCH);
+                i.putExtra(SearchManager.QUERY, artistName.getText() + " " + name.getText())
+                        .setPackage("com.google.android.apps.youtube.music");
                 //i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://music.youtube.com/search?query=" + artistName.getText() + " " + name.getText()) );
                 break;
             case R.id.buttonAmazon:
-                 i = new Intent("android.intent.action.MEDIA_SEARCH" );
-                 i.putExtra(SearchManager.QUERY, artistName.getText() + " " + name.getText())
-                         .putExtra(MediaStore.EXTRA_MEDIA_ARTIST,artistName.getText())
-                         .putExtra(MediaStore.EXTRA_MEDIA_TITLE,artistName.getText())
-                 .setPackage("com.amazon.mp3");
+                i = new Intent("android.intent.action.MEDIA_SEARCH");
+                i.putExtra(SearchManager.QUERY, artistName.getText() + " " + name.getText())
+                        .putExtra(MediaStore.EXTRA_MEDIA_ARTIST, artistName.getText())
+                        .putExtra(MediaStore.EXTRA_MEDIA_TITLE, artistName.getText())
+                        .setPackage("com.amazon.mp3");
                 //i = new Intent(Intent.ACTION_VIEW, Uri.parse("amzn://mp3/" + artistName.getText() + " " + name.getText()) );
                 break;
-
+            case R.id.buttonDDG:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://duckduckgo.com/?q=+" + artistName.getText() + " +" + name.getText()));
+                // ne fonctionne pas toujours backup = Uri.parse("https://www.deezer.com/search/" + artistName.getText() + " " + name.getText());
+                break;
 
         }
 
@@ -240,11 +254,11 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
             if (i.resolveActivity(getPackageManager()) != null) {
                 startActivity(i);
                 //this.finish();
-            }else{
-                if (backup !=  null){
-                    i = new Intent(Intent.ACTION_VIEW,backup);
+            } else {
+                if (backup != null) {
+                    i = new Intent(Intent.ACTION_VIEW, backup);
                     startActivity(i);
-                }else {
+                } else {
                     Toast.makeText(this, R.string.app_not_installed, Toast.LENGTH_SHORT).show();
                 }
             }
@@ -252,12 +266,12 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
 
     }
 
-    private void hideButton(int id){
-        findViewById(id).setVisibility(View.INVISIBLE);
+    private void hideButton(int id) {
+        findViewById(id).setVisibility(View.GONE);
     }
 
-    private void hideSourceProvider(String name){
-        switch (name){
+    private void hideSourceProvider(String name) {
+        switch (name) {
             case PROVIDER_SPOTIFY:
                 hideButton(R.id.buttonSpot);
                 break;
@@ -266,4 +280,22 @@ public class MainActivity extends AppCompatActivity implements LoadImageTask.Lis
                 break;
         }
     }
+
+    private boolean isAppLaunchable(String packageName){
+        // App is not launchable if not installed or disabled
+        Log.d(TAG, "isAppLaunchable: checking " + packageName);
+        try {
+            int result = getPackageManager().getApplicationEnabledSetting(packageName);
+            return !(result>0);
+        }catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private void hideNotInstalledApps() {
+        if(!isAppLaunchable(PACKAGE_DEEZER)){ hideButton(R.id.buttonDeez);}
+        if(!isAppLaunchable(PACKAGE_AMAZON_MUSIC)){ hideButton(R.id.buttonAmazon);}
+        if(!isAppLaunchable(PACKAGE_YOUTUBE_MUSIC)){ hideButton(R.id.buttonYM);}
+    }
+
 }
